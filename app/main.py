714085af2ccbf93d5d12a22af17960ee76286ee0
allanribeiro91 <akosr91@gmail.com>
2025-04-12 19:33:25 -0300

    criação do projeto

--- app/main.py
+++ app/main.py
@@ -1,8 +1,15 @@
 # main.py
+import json
+import os
 from flask import Flask, render_template
 
 app = Flask(__name__, template_folder='templates')
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+
+    json_path = os.path.join(app.root_path, 'data', 'account.json')
+    with open(json_path, 'r', encoding='utf-8') as file:
+        accounts = json.load(file)
+    
+    return render_template('index.html', accounts=accounts)
